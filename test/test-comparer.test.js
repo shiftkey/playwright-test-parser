6c5d2d9c62de2e463dc041f4a076c35b95e44b9b
Brendan Forster <github@brendanforster.com>
2021-08-22 13:03:54 -0300

    fix(tests): added another test

--- test/test-comparer.test.js
+++ test/test-comparer.test.js
@@ -48,4 +48,70 @@ describe('comparison', () => {
       },
     ])
   })
+
+  it('returns slow test when found in nested suite', () => {
+    const json = {
+      suites: [
+        {
+          specs: [
+            {
+              title: 'Some test',
+              line: 20,
+              tests: [
+                {
+                  projectName: 'something',
+                  results: [
+                    {
+                      /** @type {'passed'} */
+                      status: 'passed',
+                      duration: 1000,
+                    },
+                  ],
+                },
+              ],
+            },
+          ],
+          line: 10,
+          title: 'Testing things',
+          file: 'some-test.spec.ts',
+          suites: [
+            {
+              specs: [
+                {
+                  title: 'A slower test',
+                  line: 50,
+                  tests: [
+                    {
+                      projectName: 'something',
+                      results: [
+                        {
+                          /** @type {'passed'} */
+                          status: 'passed',
+                          duration: 2000,
+                        },
+                      ],
+                    },
+                  ],
+                },
+              ],
+              line: 40,
+              title: 'Testing slower things',
+              file: 'some-test.spec.ts',
+            },
+          ],
+        },
+      ],
+    }
+
+    expect(slowestTests(json, 1)).toEqual([
+      {
+        duration: 2000,
+        file: 'some-test.spec.ts',
+        lineNumber: 50,
+        specTitle: 'Testing slower things',
+        status: 'passed',
+        testTitle: 'A slower test',
+      },
+    ])
+  })
 })
