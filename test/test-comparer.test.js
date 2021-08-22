d6ee0892fe3d872f819fcfd6ff833f9e3ff5fe9f
Brendan Forster <github@brendanforster.com>
2021-08-22 13:00:57 -0300

    fix(tests): add test for other area

--- test/test-comparer.test.js
+++ test/test-comparer.test.js
@@ -7,4 +7,45 @@ describe('comparison', () => {
     }
     expect(slowestTests(json, 1)).toHaveLength(0)
   })
+
+  it('returns one test when single test found', () => {
+    const json = {
+      suites: [
+        {
+          specs: [
+            {
+              title: 'Some test',
+              line: 20,
+              tests: [
+                {
+                  projectName: 'something',
+                  results: [
+                    {
+                      /** @type {'passed'} */
+                      status: 'passed',
+                      duration: 1000,
+                    },
+                  ],
+                },
+              ],
+            },
+          ],
+          line: 10,
+          title: 'Testing things',
+          file: 'some-test.spec.ts',
+        },
+      ],
+    }
+
+    expect(slowestTests(json, 1)).toEqual([
+      {
+        duration: 1000,
+        file: 'some-test.spec.ts',
+        lineNumber: 20,
+        specTitle: '',
+        status: 'passed',
+        testTitle: 'Some test',
+      },
+    ])
+  })
 })
